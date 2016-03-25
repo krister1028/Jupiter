030674b9c062072476cc6c8e8a7d4a3a670c8423
Krister Bowman <krister.bowman@gmail.com>
2016-03-25 16:07:04 -0700

    update DRF permissions

--- jupiter/settings.py
+++ jupiter/settings.py
@@ -107,6 +107,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # DRF settings
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)
 }
