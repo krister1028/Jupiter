ea7685781ba7456e7db5cbbc383f03699be80f30
Krister Bowman <krister.bowman@gmail.com>
2016-03-25 16:01:35 -0700

    add DRF

--- jupiter/urls.py
+++ jupiter/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rest_framework import routers
+
 from scheduler import views
 
 admin.site.site_header = 'Jupiter Admin'
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,6 +1,18 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+
+from rest_framework import viewsets
+
+from scheduler.serializers import UserSerializer
 
 
 def index(request):
     return render(request, 'index.html')
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
