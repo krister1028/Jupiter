515a038494b3cc956e611c46cb27a9b56c6a60d2
Krister Bowman <krister.bowman@gmail.com>
2016-03-25 13:01:58 -0700

    update admin title

--- jupiter/urls.py
+++ jupiter/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.site.site_header = 'Jupiter Admin'
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
