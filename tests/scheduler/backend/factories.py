5014c8003aa84cc66b5785bf0d37da0d9f367d89
Krister Bowman <krister.bowman@gmail.com>
2016-06-07 16:24:34 -0700

    start building out factories

--- tests/scheduler/backend/factories.py
+++ tests/scheduler/backend/factories.py
@@ -1,7 +1,46 @@
 import factory
-from scheduler.models import Job
+import factory.fuzzy as fuzzy
+from django.contrib.auth.models import Group
+
+from scheduler.models import Job, JobTask, Product, ProductTask, JobStatus
 
 
-class JobFactory(factory.Factory):
+class GroupFactory(factory.DjangoModelFactory):
+    name = fuzzy.FuzzyText()
+
+    class Meta:
+        model = Group
+
+
+class ProductFactory(factory.DjangoModelFactory):
+    group = factory.SubFactory(GroupFactory)
+    description = fuzzy.FuzzyText()
+
+    class Meta:
+        model = Product
+
+
+class JobStatusFactory(factory.DjangoModelFactory):
+    group = factory.SubFactory(GroupFactory)
+
+    class Meta:
+        model = JobStatus
+
+
+class JobFactory(factory.DjangoModelFactory):
+    group = factory.SubFactory(GroupFactory)
+    product = factory.SubFactory(ProductFactory)
+    status = factory.SubFactory(JobStatusFactory)
+
     class Meta:
         model = Job
+
+
+class JobTaskFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = JobTask
+
+
+class ProductTaskFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = ProductTask
