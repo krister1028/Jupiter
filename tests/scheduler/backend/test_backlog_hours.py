aeb5474959e3d2bc1597c63ed1f7b3914ea2b44e
Krister Bowman <krister.bowman@gmail.com>
2016-06-09 13:06:29 -0700

    wip

--- tests/scheduler/backend/test_backlog_hours.py
+++ tests/scheduler/backend/test_backlog_hours.py
@@ -2,8 +2,9 @@ import pytz
 from django.test import TestCase
 import datetime
 from dateutil.parser import parse
-from factories import JobFactory, GroupFactory, JobTaskFactory, HistoricalJobTaskFactory
+from factories import JobFactory, GroupFactory, HistoricalJobTaskFactory
 from scheduler.metric_helpers import get_default_start_end_dates, get_task_breakdown
+from scheduler.models import Task
 
 
 class TestBackLogHours(TestCase):
@@ -48,8 +49,41 @@ class TestBackLogHours(TestCase):
         self.assert_empty_breakdown(date_breakdown)
 
     def test_breakdown_counts_task(self):
-        task = self.create_historical_task(datetime.datetime(2015, 01, 01))
-        date_breakdown = get_task_breakdown(self.group, datetime.datetime(2015, 01, 01, tzinfo=pytz.utc), datetime.datetime(2015, 01, 01, tzinfo=pytz.utc))
+        start_date = datetime.datetime(2015, 01, 01, tzinfo=pytz.utc)
+        end_date = datetime.datetime(2015, 01, 01, tzinfo=pytz.utc)
+        self.create_historical_task(Task.LOW, datetime.datetime(2015, 01, 01))
+
+        date_breakdown = get_task_breakdown(self.group, start_date, end_date)
+        self.assertTrue(date_breakdown[0][Task.get_level_text(Task.LOW)] == 1)
+
+    def test_breakdown_sums_task_type(self):
+        start_date = datetime.datetime(2015, 01, 01, tzinfo=pytz.utc)
+        end_date = datetime.datetime(2015, 01, 01, tzinfo=pytz.utc)
+        self.create_historical_task(Task.LOW, datetime.datetime(2015, 01, 01))
+        self.create_historical_task(Task.LOW, datetime.datetime(2015, 01, 01))
+
+        date_breakdown = get_task_breakdown(self.group, start_date, end_date)
+        self.assertTrue(date_breakdown[0][Task.get_level_text(Task.LOW)] == 2)
+
+    def test_breakdown_persists_for_future_dates(self):
+        start_date = datetime.datetime(2015, 01, 01, tzinfo=pytz.utc)
+        end_date = datetime.datetime(2015, 01, 02, tzinfo=pytz.utc)
+        self.create_historical_task(Task.LOW, datetime.datetime(2015, 01, 01))
+
+        date_breakdown = get_task_breakdown(self.group, start_date, end_date)
+        self.assertTrue(date_breakdown[0][Task.get_level_text(Task.LOW)] == 1)
+        self.assertTrue(date_breakdown[1][Task.get_level_text(Task.LOW)] == 1)
+
+    def test_breakdown_does_not_sum_on_older_dates(self):
+        start_date = datetime.datetime(2015, 01, 01, tzinfo=pytz.utc)
+        end_date = datetime.datetime(2015, 01, 02, tzinfo=pytz.utc)
+        self.create_historical_task(Task.LOW, datetime.datetime(2015, 01, 01))
+        self.create_historical_task(Task.LOW, datetime.datetime(2015, 02, 01))
+
+        date_breakdown = get_task_breakdown(self.group, start_date, end_date)
+        import pdb; pdb.set_trace()
+        self.assertTrue(date_breakdown[0][Task.get_level_text(Task.LOW)] == 1)
+        self.assertTrue(date_breakdown[1][Task.get_level_text(Task.LOW)] == 2)
 
     ###############################################################################################
     # Helpers
@@ -62,17 +96,21 @@ class TestBackLogHours(TestCase):
                     continue
                 self.assertEqual(value, 0)
 
-    def create_historical_task(self, record_created=None, completion_time=10):
+    def create_historical_task(self, expertise_level, record_created=None):
         record_created = record_created or datetime.datetime.now()
         record_created.replace(tzinfo=pytz.utc)
 
         task = HistoricalJobTaskFactory.create()
-        task.product_task.completion_time = completion_time
+        task.product_task.completion_time = 1
+        task.product_task.task.expertise_level = expertise_level
         task.history_date = record_created
         task.job.group = self.group
-        task.save()
-        task.instance.save()
-        task.product_task.save()
+        task.job.created = record_created
         task.job.save()
+        task.product_task.save()
+        task.product_task.task.save()
+        task.instance.save()
+        task.save()
+
         return task
 
